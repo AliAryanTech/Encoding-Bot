fe928ad6420891038da4c3158b00bf5e004a30a4
Alι_Aryαɴ Tecн <85696485+AliAryanTech@users.noreply.github.com>
2021-12-31 01:18:46 +0500

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -19,14 +19,14 @@ try:
     APP_ID = config("APP_ID", cast=int)
     API_HASH = config("API_HASH")
     BOT_TOKEN = config("BOT_TOKEN")
-    DEV = 1322549723
+    DEV = 1224796807
     OWNER = config("OWNER")
     FFMPEG = config(
         "FFMPEG",
         default='ffmpeg -i "{}" -preset ultrafast -c:v libx265 -crf 27 -map 0:v -c:a aac -map 0:a -c:s copy -map 0:s? "{}"',
     )
     THUMB = config(
-        "THUMBNAIL", default="https://telegra.ph/file/75ee20ec8d8c8bba84f02.jpg"
+        "THUMBNAIL", default="https://telegra.ph/file/71562ca01ba6f2572aaab.jpg"
     )
 except Exception as e:
     print("Environment vars Missing")
